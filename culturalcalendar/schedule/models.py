3c1d680b6c7be7236fb8b4813c3cf1c6bb86c76f
rafaelchiafarelli <rafael.chiafarelli@gmail.com>
2018-08-22 14:07:45 -0300

    files changed

--- culturalcalendar/schedule/models.py
+++ culturalcalendar/schedule/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Question(models.Model):
@@ -13,3 +14,26 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class CulturalEvent(models.Model):
+    CE_name = models.CharField(max_length=250)
+    CE_location = models.CharField(max_length=250)
+    CE_date_and_time = models.DateTimeField(auto_now = True, auto_now_add = False)
+    CE_value = models.CharField(max_length=250)
+    CE_description = models.CharField(max_length=250)
+    CE_file = models.ImageField(null = True, blank = True, upload_to='uploads')
+    
+    def __str__(self):
+        return self.CE_name
+    def __unicode__(self):
+        return self.CE_name
+    def get_absolute_url(self):
+        return reverse("CulturalEvent:details", kwargs={"id":self.id})
+    
+class CulturalEventStatistics(models.Model):
+    CE_father = models.ForeignKey(CulturalEvent, on_delete=models.CASCADE)
+    
+    
+class CulturalEventChild(models.Model):
+    CE_father = models.ForeignKey(CulturalEvent, on_delete=models.CASCADE)
+    
--- culturalcalendar/schedule/urls.py
+++ culturalcalendar/schedule/urls.py
@@ -2,6 +2,15 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'schedule'
+
 urlpatterns = [
-    url('', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+
+    #createevent/add
+    url(r'culturalevent/add/$', views.CreateEvent.as_view(), name='createevent-add'),
+    
+    #view specific culturalevent
+    url(r'^(?P<pk>[0-9]+)$', views.DetailView.as_view(), name='detail'),
+    
 ]
